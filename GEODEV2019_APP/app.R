00d304aba144fecf2297693050e0d88036a370c5
Ben <ben@Bens-MacBook-Air.local>
2019-03-04 13:12:22 -0500

    ready

--- GEODEV2019_APP/app.R
+++ GEODEV2019_APP/app.R
@@ -8,6 +8,41 @@
 #
 
 library(shiny)
+library(sf)
+library(dplyr)
+library(leaflet)
+
+amtrak <- read_sf('data/amtrak_rails/amtrak_rails.shp')
+#rs72017
+amtrak_cbsa <- read_sf("data/amtrak_cbsa/amtrak_cbsa.shp")
+conus_regions <- read_sf("data/conus_regions/conus_regions.shp")
+
+center_conus <- st_centroid(conus_regions %>% st_union()) %>%
+  st_coordinates() 
+
+pal <- colorNumeric(
+  palette = c("#edf8b1", "#2c7fb8"),
+  domain = amtrak_cbsa$rs72017)
+
+m <- leaflet() %>%
+  addTiles()  %>%
+  addPolygons(data = amtrak_cbsa,  
+              fillColor = ~pal(rs72017),
+              fillOpacity = .8,
+              color = 'black', 
+              weight = 1,
+              label = ~paste(NAME, "'s Expected Population for 2017 was", rs72017)
+  ) %>%
+  addLegend("bottomleft", 
+            data = amtrak_cbsa,
+            pal = pal, 
+            values = ~rs72017,
+            title = "Est. Population (2017)",
+            opacity = 1
+  ) %>%
+  addPolylines(data = amtrak,
+               color = 'red', 
+               weight = 3)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -18,16 +53,14 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         selectInput("centroid",
+                     "Choose Centroid",
+                     choices = c('CONUS', 'South', 'Northeast', 'West', 'North Central'))
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         leafletOutput("map")
       )
    )
 )
@@ -35,13 +68,26 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
+   output$map <- renderLeaflet({
+     if(input$centroid == 'CONUS'){
+       myCentroid <- center_conus
+       
+       myZoom = 3
+     } else {
+       print(input$centroid)
+       print(conus_regions %>% dplyr::filter(REGION == input$centroid))
+       myCentroid <- conus_regions %>%
+         dplyr::filter(REGION == input$centroid) %>%
+         st_centroid() %>%
+         st_coordinates()
+       
+       myZoom <- 5
+     }
+     
+     print(myCentroid)
       # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      m %>%
+        setView(center_conus[1], center_conus[2], myZoom)
    })
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#GEODEV 2019
+#GEODEV 2019 (https://tinyurl.com/r-geodev2019)
 
 A cloud environment to follow along with the lesson can be found here [https://rstudio.cloud/project/231866](https://rstudio.cloud/project/231866). You will need to create an account or log in with Google or Github.
 
-Alternatively, you can also follow along on your own machine. Make sure to have the following installed: *install.packages('tidyverse', 'sf', 'spData')* You will also need to download the data files as well. 
+Alternatively, you can also follow along on your own machine. Make sure to have the following installed: *install.packages('tidyverse', 'sf', 'spData', 'leaflet')* You will also need to download the data files as well. 
